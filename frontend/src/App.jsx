05d14c95e25b77f1f9e707fd6dc645e64f929771
Rishi8603 <rishii8603@gmail.com>
2025-10-22 18:09:58 +0530

    app.jsx fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Authpage from "./pages/Authpage";
-import Layout from './components/Layout';
+import Layout from "./components/Layout";
 import Homepage from "./pages/Homepage";
 import UserProfile from "./pages/UserProfile";
 import Settings from "./pages/Settings";
