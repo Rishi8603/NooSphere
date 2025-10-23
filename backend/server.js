00cb09aab19424a024ead1825066fc214738dd2f
Rishi8603 <rishii8603@gmail.com>
2025-10-23 09:29:37 +0530

    health check endpoint add

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,15 @@ const PORT = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+
+app.get('/api/health', (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    message: "Backend running",
+    timestamp: new Date().toISOString()
+  });
+});
+
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/posts',require('./routes/posts'));
 app.use('/api/upload',require('./routes/upload'));
